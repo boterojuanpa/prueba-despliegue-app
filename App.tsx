e493a78caa7d331182e9ba08b4a3d53dcd86e1a5
Juan Pablo Botero <juan.botero@ceiba.com.co>
2020-04-15 12:39:40 -0500

    proyecto inicial

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>App de prueba Juan Botero...</Text>      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const styles1 = StyleSheet.create({
+  container: {
+    fontSize: 20
+  },
+});
